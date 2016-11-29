faf0773f08160686a47d4dbe4711f09f98a39650
NancyZ <zhaon743@newschool.edu>
2016-11-29 20:35:04 +0000

    write to json update

--- week11/query.js
+++ week11/query.js
@@ -1,26 +1,19 @@
 var pg = require('pg');
 var http = require('http');
-
-
-var un = process.env.USERNAME; // aws db username
-var pw = process.env.PASSWORD; // aws db password
-var db = process.env.DB_NAME; // aws db database name
-var ep = process.env.DB_END_POINT; // aws db endpoint
-var conString = "postgres://" + un + ":" + pw + "@" + ep + "/" + db;
+var fs = require('fs');
 
 // connection string
-// var un = 'NancyZ'; // aws db username
-// var pw = 'password'; // aws db password
-// var db = 'theBeatMap'; // aws db database name
-// var ep = 'thebeatmap.ck2shkhomros.us-east-1.rds.amazonaws.com:5432'; // aws db endpoint
-// var conString = "postgres://" + un + ":" + pw + "@" + ep + "/" + db;
+var un = 'NancyZ'; // aws db username
+var pw = 'password'; // aws db password
+var db = 'theBeatMap'; // aws db database name
+var ep = 'thebeatmap.ck2shkhomros.us-east-1.rds.amazonaws.com:5432'; // aws db endpoint
+var conString = "postgres://" + un + ":" + pw + "@" + ep + "/" + db;
 console.log(conString);
 
-var createTableQuery = "CREATE TABLE music (genre varchar(50), metronomeBpm smallint, monthCreated timestamp DEFAULT current_timestamp);"
-var insertIntoQuery = "INSERT INTO music VALUES ('R&B', 73, DEFAULT);"
-var query = "SELECT * FROM music;" //sum(amount), avg(amount), comma is used to separate columns of select
-//var complexQuery = "SELECT sum(amount) as total FROM wham GROUP BY whammy;"
-var del = "DROP TABLE music;"
+//var createTableQuery = "CREATE TABLE music (genre varchar(50), metronomeBpm smallint, monthCreated timestamp DEFAULT current_timestamp);"
+//var insertIntoQuery = "INSERT INTO music VALUES ('R&B', 73, DEFAULT);"
+var query = "SELECT * FROM music;" 
+//var del = "DROP TABLE music;"
 
 var server = http.createServer(function(req, res) {
     console.log('server started');
@@ -41,6 +34,9 @@ var server = http.createServer(function(req, res) {
             res.writeHead(200, {'content-type': 'application/json'});
             res.write(JSON.stringify(result.rows));
             res.end();
+            
+            fs.writeFileSync('output.json', JSON.stringify(result.rows));
+            
             
         });
     });
